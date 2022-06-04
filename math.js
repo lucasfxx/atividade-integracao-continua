96c348b1fb33c4da161e80b7b80e6de7dcec3ae6
Lucas Felipe <lucasfxy18@gmail.com>
2022-06-04 09:42:38 -0300

    teste

--- math.js
+++ math.js
@@ -1,13 +1,26 @@
 function soma(a, b) {
+    return a + b
 }
+
+console.log(soma(2,4))
+
 function subtracao(a, b) {
+    return a - b
 }
+
+console.log(soma(4,2))
+
 
 function divisao(a, b) {
+return a / b
 }
+console.log(soma(4,2))
 
 function multiplicacao(a, b) {
+    return a * b
 }
+console.log(soma(4,2))
+
 
 module.exports = {
     soma,
